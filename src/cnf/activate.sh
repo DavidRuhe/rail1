591d051757a57a16b7239cdbbb929e9fd1bed185
David Ruhe <d.ruhe@uva.nl>
2024-02-29 16:53:33 +0100

    activate updates

--- src/cnf/activate.sh
+++ src/cnf/activate.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 # set -e
 
-# Specify which Anaconda environment to use.
-check_variable() {
-    if [ -z "${ENV_FILE}" ]; then
-        echo "Environment variable ENV_FILE is not set."
-        return 1 # Return with a non-zero status to indicate failure
-    fi
-}
-check_variable || return
+HOSTNAME=$(hostname)
+ROOT=$(git rev-parse --show-toplevel)
 
-export DATAROOT=$HOME/datasets/
+if [ $? -ne 0 ]; then
+    echo "Could not find root directory. Exiting."
+    return 1
+fi
 
-ROOT='../..'
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+
+if [ "$HOSTNAME" == "c3230" ]; then
+    export DATAROOT=$HOME/datasets/
+    export ENV_FILE=$ROOT/env/py310_pyg230_cu115.yaml
+elif [ "$HOSTNAME" == "gcn*" ]; then
+    export DATAROOT=$HOME/datasets/
+    export ENV_FILE=$ROOT/env/py310_pyg230_cu115.yaml
+else
+    echo "Unknown hostname $HOSTNAME". Exiting.
+    return 1
+fi
+
 ENV_NAME=$(grep 'name:' $ENV_FILE | cut -d ' ' -f 2)
 
 conda env list | grep -q "^$ENV_NAME\s"
 if [ $? -ne 0 ]; then
-    echo "Creating conda environment $ENV_NAME"
-    conda env create -f $ENV_FILE
+    echo "Environment $ENV_NAME not found. Exiting."
+    return 1
+else 
+    conda activate $ENV_NAME
 fi
-conda activate $ENV_NAME
 
 VENV_DIR='.venv'
 if [ ! -d "$VENV_DIR" ]; then
     echo "Creating virtual environment $VENV_DIR"
-    python3 -m venv $VENV_DIR
+    python3 -m venv $VENV_DIR --system-site-packages
     source $VENV_DIR/bin/activate
     pip install -r requirements.txt
-    # cd $ROOT && pip install -e . && cd -
+    cd $ROOT && pip install -e . && cd -
 else
     source $VENV_DIR/bin/activate
 fi
