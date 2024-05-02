574742f4881ec7eb8f3cfce08a7a935c9d5c6dca
David Ruhe <d.ruhe@uva.nl>
2024-05-02 10:31:03 +0200

    brjquzss

--- src/cnf/config/cfg_random_shapes_rec.py
+++ src/cnf/config/cfg_random_shapes_rec.py
@@ -32,9 +32,9 @@ config = {
     # ),
     "fit": {"max_steps": 100_000, "print_interval": 32, "limit_val_batches": float('inf')},
     "parameters": {"seed": {"values": [0]}},
-    # "cluster": dict(
-    #     address="snellius",
-    #     directory="/home/druhe/rail1/src/cnf/",
-    #     slurm="--partition=gpu --time=24:00:00 --gpus-per-node=1",
-    # ),
+    "cluster": dict(
+        address="snellius",
+        directory="/home/druhe/rail1/src/cnf/",
+        slurm="--partition=gpu --time=24:00:00 --gpus-per-node=1",
+    ),
 }
